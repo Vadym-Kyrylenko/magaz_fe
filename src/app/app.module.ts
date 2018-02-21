a19b4538bd3840cb978e40c2f5b5552809ee0db7
Vadym Kyrylenko <kyrylenko.vadym@gmail.com>
2018-02-21 18:11:53 +0200

    basic structure and routing was implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {ShopComponent} from "./shop/shop.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
+import {ShopContentComponent} from "./shop-content/shop-content.component";
 
+import { AdminComponent} from "./admin/admin.component";
+
+
+// const homeRoutes: Routes = [
+//   //{ path: '', component: ShopComponent},
+//   { path: 'admin', component: AdminComponent}
+//   // { path: 'contacts', component: ContactsComponent}
+//   // { path: 'callbackbtn', component: CallBackComponent},
+// ];
+
+const appRoutes: Routes = [
+  { path: '', component: ShopComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ShopComponent, FooterComponent, HeaderComponent, ShopContentComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
