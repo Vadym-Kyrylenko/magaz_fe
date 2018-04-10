9fb51a05aa6b39c510b7d43c91e4dd22ba23074e
Vadym Kyrylenko <kyrylenko.vadym@gmail.com>
2018-04-10 17:27:49 +0300

    adaptation for mobile devices

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,6 +1,6 @@
 export const urlsettings = {
-  backurl: 'http://localhost:3000',
+  // backurl: 'http://localhost:3000',
   // backurl: 'http://192.168.88.247:3000'
-  // backurl: 'http://46.172.70.39:3000',
+  backurl: 'http://46.172.70.39:3000',
 
 };
